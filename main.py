8c1e0838fd93aa2e161531a3af1db5b6da4b016e
ryuga <vaishnav2353@gmail.com>
2020-10-05 01:33:53 +0530

    correct liner errors

--- main.py
+++ main.py
@@ -1,34 +1,42 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-   return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/about')
 def about():
-   return render_template('about.html')
+    return render_template('about.html')
+
 
 @app.route('/tourist')
 def contact():
-   return render_template("tourist.html")
+    return render_template("tourist.html")
+
 
 @app.route('/dance')
 def dance():
-   return render_template("dance.html")
+    return render_template("dance.html")
+
 
 @app.route('/feedback')
 def feedback():
-   return render_template("feedback.html")
+    return render_template("feedback.html")
+
 
 @app.route('/festival')
 def festival():
-   return render_template("festival.html")
+    return render_template("festival.html")
+
 
 @app.route('/submit')
 def submit():
-   return render_template("submit.html")
+    return render_template("submit.html")
+
 
 @app.route('/food')
 def food():
-   return render_template("food.html")
+    return render_template("food.html")
